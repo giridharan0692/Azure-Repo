f8e3e42cfb9e6b71fd0cac278b2a12a295028002
Udhayakumar <Giridharan_Udhayakumar@premierinc.com>
2025-11-25 04:19:45 -0500

    removed secret

--- Terraform-Templates/main.tf
+++ Terraform-Templates/main.tf
@@ -11,8 +11,5 @@ provider "azurerm" {
     features {}
     ## Add your Azure RM provider configuration here
     ## Example:
-  subscription_id = ""
-  tenant_id       = ""
-  client_id       = ""
-  client_secret   = ""
+
 }
